7c9a34531e3f1d801ad512229e6c21449aa0e1f6
K0IN <thisk0in@gmail.com>
2024-09-12 01:32:25 +0200

    init

---
+++ go.mod
@@ -0,0 +1,47 @@
+module github.com/k0in/wsl-docker/v2
+
+go 1.22
+
+toolchain go1.22.7
+
+require (
+	github.com/docker/docker v27.2.1+incompatible
+	github.com/opencontainers/image-spec v1.1.0
+	github.com/urfave/cli v1.22.15
+	github.com/yuk7/wsllib-go v1.0.0
+)
+
+require (
+	github.com/AdaLogics/go-fuzz-headers v0.0.0-20240806141605-e8a1dd7889d6 // indirect
+	github.com/Microsoft/go-winio v0.4.14 // indirect
+	github.com/containerd/log v0.1.0 // indirect
+	github.com/cpuguy83/go-md2man/v2 v2.0.4 // indirect
+	github.com/distribution/reference v0.6.0 // indirect
+	github.com/docker/go-connections v0.5.0 // indirect
+	github.com/docker/go-units v0.5.0 // indirect
+	github.com/felixge/httpsnoop v1.0.4 // indirect
+	github.com/go-logr/logr v1.4.2 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/klauspost/compress v1.17.9 // indirect
+	github.com/moby/docker-image-spec v1.3.1 // indirect
+	github.com/moby/patternmatcher v0.6.0 // indirect
+	github.com/moby/sys/sequential v0.6.0 // indirect
+	github.com/moby/sys/user v0.3.0 // indirect
+	github.com/moby/sys/userns v0.1.0 // indirect
+	github.com/moby/term v0.5.0 // indirect
+	github.com/morikuni/aec v1.0.0 // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.54.0 // indirect
+	go.opentelemetry.io/otel v1.30.0 // indirect
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp v1.30.0 // indirect
+	go.opentelemetry.io/otel/metric v1.30.0 // indirect
+	go.opentelemetry.io/otel/sdk v1.30.0 // indirect
+	go.opentelemetry.io/otel/trace v1.30.0 // indirect
+	golang.org/x/sys v0.25.0 // indirect
+	golang.org/x/time v0.6.0 // indirect
+	gotest.tools/v3 v3.5.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,211 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types/image"
+	"github.com/docker/docker/api/types/network"
+	"github.com/docker/docker/client"
+	"github.com/docker/docker/pkg/archive"
+	v1 "github.com/opencontainers/image-spec/specs-go/v1"
+	"github.com/urfave/cli"
+	"github.com/yuk7/wsllib-go"
+)
+
+func buildDocker(dockerFilePath string) (imageName *string, err error) {
+	imgName := "tmp-image:latest"
+	apiClient, err := client.NewClientWithOpts(client.FromEnv)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create Docker client: %v", err)
+	}
+	defer apiClient.Close()
+
+	fullPath, err := filepath.Abs(dockerFilePath)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get dockerfile path: %v", err)
+	}
+
+	ctx, err := archive.TarWithOptions(fullPath, &archive.TarOptions{})
+	if err != nil {
+		return nil, fmt.Errorf("failed to create tar: %v", err)
+	}
+
+	dockerFileName := filepath.Base(dockerFilePath)
+	res, err := apiClient.ImageBuild(context.Background(), ctx, types.ImageBuildOptions{Tags: []string{imgName}, Remove: false, Dockerfile: dockerFileName})
+	if err != nil {
+		return nil, fmt.Errorf("imageBuild failed: %v", err)
+	}
+
+	if _, err = io.Copy(os.Stdout, res.Body); err != nil {
+		return nil, fmt.Errorf("failed to read response body: %v", err)
+	}
+
+	if err = res.Body.Close(); err != nil {
+		return nil, fmt.Errorf("failed to close response body: %v", err)
+	}
+	return &imgName, nil
+}
+
+func pullDockerImage(imageName string) (imgName *string, err error) {
+	apiClient, err := client.NewClientWithOpts(client.FromEnv)
+	if err != nil {
+		return nil, err
+	}
+	defer apiClient.Close()
+	res, err := apiClient.ImagePull(context.Background(), imageName, image.PullOptions{})
+	if err != nil {
+		return nil, err
+	}
+	defer res.Close()
+	return &imageName, nil
+}
+
+func exportDockerImage(imageName string) (err error) {
+	containerName := "container"
+	apiClient, err := client.NewClientWithOpts(client.FromEnv)
+	if err != nil {
+		return fmt.Errorf("failed to create Docker client: %v", err)
+	}
+	defer apiClient.Close()
+
+	createdContainer, err := apiClient.ContainerCreate(context.Background(),
+		&container.Config{Image: imageName},
+		&container.HostConfig{},
+		&network.NetworkingConfig{},
+		&v1.Platform{},
+		containerName)
+
+	if err != nil {
+		return fmt.Errorf("failed to create container: %v", err)
+	}
+
+	reader, exportError := apiClient.ContainerExport(context.Background(), createdContainer.ID)
+
+	if err = apiClient.ContainerRemove(context.Background(), createdContainer.ID, container.RemoveOptions{Force: true, RemoveVolumes: true}); err != nil {
+		return fmt.Errorf("failed to remove container: %v", err)
+	}
+
+	if exportError != nil {
+		return fmt.Errorf("failed to export container: %v", exportError)
+	}
+
+	defer reader.Close()
+
+	imageFile, err := os.Create("image.tar")
+	if err != nil {
+		return fmt.Errorf("failed to create image file: %v", err)
+	}
+
+	defer imageFile.Close()
+
+	if _, err = io.Copy(imageFile, reader); err != nil {
+		return fmt.Errorf("failed to copy image file: %v", err)
+	}
+
+	return nil
+}
+
+func importWsl(distroName string) (err error) {
+	_ = wsllib.WslUnregisterDistribution(distroName)
+	path, err := filepath.Abs("image.tar")
+	if err != nil {
+		return err
+	}
+
+	cmd := exec.Command("wsl", "--import", distroName, ".", "image.tar", "--version", "2")
+	res, err := cmd.Output()
+	if err != nil {
+		fmt.Printf("failed to import WSL: %v, %s\n", err, string(res))
+		return wsllib.WslRegisterDistribution(distroName, path)
+	}
+
+	return nil
+}
+
+func launchWsl() (err error) {
+	_, err = wsllib.WslLaunchInteractive("DistroName", "", true)
+	return err
+}
+
+func main() {
+	app := &cli.App{
+		Name:  "Docker 2 WSL",
+		Usage: "Convert a Docker image to a WSL distribution",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:  "distro-name",
+				Usage: "the name of the WSL distribution",
+				Value: "dist",
+			},
+			&cli.StringFlag{
+				Name:  "image",
+				Usage: "The name of the image or 'Dockerfile' if you want to build the Dockerfile in your current directory",
+				Value: "Dockerfile",
+			},
+			&cli.BoolFlag{
+				Name:  "launch",
+				Usage: "Launch the WSL distribution after importing",
+			},
+		},
+		Action: func(c *cli.Context) error {
+			var ImageName = c.String("image")
+			var distroName = c.String("distro-name")
+
+			_, err := os.Stat(ImageName)
+			hasDockerfile := err == nil
+			var img *string
+
+			if !hasDockerfile {
+				println("Pulling Docker image...")
+				img, err = pullDockerImage(ImageName)
+				if err != nil {
+					return fmt.Errorf("failed to pull Docker image: %v", err)
+				}
+			} else {
+				println("Building Docker image...")
+				img, err = buildDocker(ImageName)
+				if err != nil {
+					return fmt.Errorf("failed to build Docker image: %v", err)
+				}
+			}
+
+			fmt.Printf("Successfully built Docker image '%s'\n", *img)
+
+			err = exportDockerImage(*img)
+			if err != nil {
+				return fmt.Errorf("failed to export Docker image: %v", err)
+			}
+
+			defer os.Remove("image.tar")
+
+			err = importWsl(distroName)
+			if err != nil {
+				return fmt.Errorf("failed to import WSL: %v", err)
+			}
+
+			fmt.Printf("Successfully imported %s to WSL\n", *img)
+			fmt.Printf("Run `wsl -d %s` to launch the distribution\n", distroName)
+
+			if c.Bool("launch") {
+				err = launchWsl()
+				if err != nil {
+					return fmt.Errorf("failed to launch WSL: %v", err)
+				}
+			}
+
+			return nil
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
